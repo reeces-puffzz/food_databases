3c4df69390d4dd27b46c02edd7ed6ca1bf20232e
Saliha Demir <91100792+SalihaDemir@users.noreply.github.com>
2023-11-17 10:52:14 -0500

    Notes before coding

---
+++ AI_Test.kt
@@ -0,0 +1,12 @@
+class AI_Test {
+
+    //Deep Learning Test
+
+    //Deep Learning for Machine Vision
+    //https://github.com/Kotlin/kotlindl-app-sample/blob/main/app/src/main/java/org/jetbrains/kotlinx/dl/example/app/ImageAnalyzer.kt
+
+    //Text recognition by camera using Kotlin!
+    //https://www.youtube.com/watch?v=teaNKLb6l4Q
+
+
+}
